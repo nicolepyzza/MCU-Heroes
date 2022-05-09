9bf30b87acd768d252ca00b855b7649966bc1414
Nicole <nicole.pyzza@gmail.com>
2022-05-09 10:11:06 -0400

    removing delete hero

--- testApp/src/app/hero.ts
+++ testApp/src/app/hero.ts
@@ -1,4 +1,13 @@
 export interface Hero {
     id: number;
     name: string;
+    img: string;
+    aliases: Array<string>;
+    species: string,
+    gender: string;
+    affiliation: Array<string>;
+    status: string;
+    skills: Array<string>;
+    details: string;
+
 }
